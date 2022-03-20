79209311087f6c5a6f2c01c550fd2538dce560c9
siddhant2002 <siddhantswarupmallick@gmail.com>
2022-03-20 22:19:50 +0530

    Number 10 updated

--- Set - 5/Number_10.java
+++ Set - 5/Number_10.java
@@ -1,9 +1,10 @@
 import java.util.*;
 class Number_10 {
     public static void main(String[] args) {
-        Scanner sc=new Scanner(System.in);
-        String s=sc.nextLine();
-        System.out.println(palin(s,"",0));
+        try (Scanner sc = new Scanner(System.in)) {
+            String s=sc.nextLine();
+            System.out.println(palin(s,"",0));
+        }
     }
     static boolean palin(String s , String p , int i)
     {
