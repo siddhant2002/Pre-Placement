388b201c7d69e84784b6a16ae370eefbb020be60
siddhant2002 <siddhantswarupmallick@gmail.com>
2022-03-20 12:39:15 +0530

    Number 5 updated

--- Set - 3/Number_5.java
+++ Set - 3/Number_5.java
@@ -1,24 +1,24 @@
 import java.util.*;
 public class Number_5{
-
      public static void main(String []args){
          Scanner sc=new Scanner(System.in);
          int n=sc.nextInt();
-         int a[] = new int[n];
+         int a[]=new int[n];
          for(int i=0;i<n;i++)
          {
              a[i]=sc.nextInt();
          }
-         HashMap<Integer,Integer> nm=new HashMap<>();
+         int x=sc.nextInt();
+         Map<Integer,Integer> nm=new HashMap<>();
+         int c=0;
          for(int i=0;i<n;i++)
          {
-             if(nm.containsKey(a[i]))
+             if(nm.containsKey(x-a[i]))
              {
-                 System.out.println(a[i]);
-                 return;
+                 c++;
              }
-             nm.put(a[i],0);
+             nm.put(a[i] , nm.getOrDefault(a[i],0)+1);
          }
-         System.out.println("Not found");
+         System.out.println(c);
      }
 }
