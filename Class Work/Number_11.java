f1fb6d91833a62c6cb1e3617796f2ec026c851fb
siddhant2002 <siddhantswarupmallick@gmail.com>
2022-03-21 14:31:35 +0530

    Number 11 updated

---
+++ Class Work/Number_11.java
@@ -0,0 +1,21 @@
+import java.util.*;
+
+public class Number_11 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        System.out.println("Numbers before Swapping: "+"a = "+a+" and b = "+b);
+        a = a+b;
+        b = a-b;
+        a = a-b;
+        System.out.println("Numbers after Swapping: "+"a = "+a+" and b = "+b);
+        int m = sc.nextInt();
+        int n = sc.nextInt();
+        System.out.println("Numbers before Swapping: "+"a = "+m+" and b = "+n);
+        n= m^n;
+        m= m^n;
+        n= m^n;
+        System.out.println("Numbers after Swapping: "+"a = "+m+" and b = "+n);
+    }
+}
